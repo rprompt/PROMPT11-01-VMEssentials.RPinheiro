82aea71528b432232e1d1e275611821574ecea17
Rui Pinheiro <rui.sena@gmail.com>
2011-05-04 23:02:37 +0100

    Exercicio 4

--- Sessao1 - CLI Part1/testep.cs
+++ Sessao1 - CLI Part1/testep.cs
@@ -8,14 +8,18 @@ namespace Sessao1
         { Ponto p1;
           Ponto p2;
           Int32 res;
+          Int32 compt;
 
             p1 = new Ponto(3,4);
             p2 = new Ponto(5,7);
 
             res = p1.Distance(p2); 
-            Console.WriteLine(p1.ToString());
-            Console.WriteLine(p2.ToString());
-            Console.WriteLine(res.ToString());
+            compt = p1.CompareTo(p2);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(res);
+            Console.WriteLine(compt);
         }
     }
 }
